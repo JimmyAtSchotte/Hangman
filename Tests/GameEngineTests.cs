c7ae1c15f1841c671b0363528472abe2626a242e
JimmyMattsson <jimmy.mattsson@rekylsystems.se>
2023-07-05 21:15:59 +0200

    Correct word test

--- Tests/GameEngineTests.cs
+++ Tests/GameEngineTests.cs
@@ -5,5 +5,20 @@ namespace Tests;
 [TestFixture]
 public class GameEngineTests
 {
-    
+    [Test]
+    public void CorrectWordGuess()
+    {
+        var game = new GameEngine();
+        var guessResult = game.Guess('a');
+        
+        Assert.IsTrue(guessResult);
+    }
+}
+
+public class GameEngine
+{
+    public bool Guess(char c)
+    {
+        return true;
+    }
 }
