82341433e2d85cd4aaf21553d193013354962cb0
JimmyMattsson <jimmy.mattsson@rekylsystems.se>
2023-07-05 21:39:10 +0200

    Guess part of word

--- Tests/HangmanEngineTests.cs
+++ Tests/HangmanEngineTests.cs
@@ -22,22 +22,42 @@ public class HangmanEngineTests
         
         Assert.IsFalse(guessResult.Victory);
     }
+    
+    [Test]
+    public void MatchedPartOfWord()
+    {
+        var game = new HangmanEngine("ab");
+        var guessResult = game.Guess('b');
+        
+        Assert.IsFalse(guessResult.Victory);
+        Assert.IsNull(guessResult.WordProgress[0]);
+        Assert.AreEqual('b', guessResult.WordProgress[1]);
+    }
 }
 
 public class HangmanEngine
 {
     private readonly char[] _correctWord;
+    private readonly char?[] _wordProgress;
 
     public HangmanEngine(string correctWord)
     {
         _correctWord = correctWord.ToCharArray();
+        _wordProgress = new char?[_correctWord.Length];
     }
 
     public GuessResult Guess(char guess)
     {
+        for (var i = 0; i < _correctWord.Length; i++)
+        {
+            if (_correctWord[i].Equals(guess))
+                _wordProgress[i] = guess;
+        }
+
         return new GuessResult()
         {
-            Victory = _correctWord.Any(c => c == guess)
+            Victory = _wordProgress.All(c => c != null),
+            WordProgress = _wordProgress
         };
     }
 }
@@ -45,4 +65,5 @@ public class HangmanEngine
 public class GuessResult
 {
     public bool Victory { get; set; }
+    public char?[] WordProgress { get; set; }
 }
